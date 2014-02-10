a742b46b1d20c929a708bdf4f75da33fa626176a
Mark Rustad <MRustad@gmail.com>
2014-02-09 20:35:02 -0800

    Add LICENSE file, and update copyright.
    Signed-off-by: Mark Rustad <MRustac@mac.com>

--- plato_if.c
+++ plato_if.c
@@ -11,11 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * Copyright © 2013 Mark Rustad <MRustad@mac.com>
+ * Copyright © 2013, 2014 Mark Rustad <MRustad@mac.com>
  *
  * Written by: Mark Rustad, 2013/01/20
  *
