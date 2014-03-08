66299b353a6e35c23e3ae0a5d76b3c3a03694ea2
Mark Rustad <MRustad@gmail.com>
2014-03-08 14:30:25 -0800

    Modify notice based on latest
    Signed-off-by: Mark Rustad <MRustad@mac.com>

--- plato_if.c
+++ plato_if.c
@@ -1,15 +1,14 @@
 /*
  * plato_if - An interface for PLATO terminals
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the license.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License version 2 for more details.
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program in the file named COPYING.
